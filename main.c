9ba33ed1d2745de80d539990117c891fb9706135
Mykhailo Sopiha <mykhailo.sopiha@linaro.org>
2019-01-08 18:47:11 +0200

    Added encrypt/decrypt stubs
    Features:
     - Added encrypt/decrypt dummy functions
    
    Signed-off-by: Mykhailo Sopiha <mykhailo.sopiha@linaro.org>

--- main.c
+++ main.c
@@ -11,13 +11,14 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <signal.h>
 
 #include "log.h"
 
 #define TCP_PORT_DEFAULT 8080
 #define BUF_LEN_DEFAULT 256
 
-pthread_mutex_t g_sock_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t g_sock_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static struct socket_fd {
     int fd;
@@ -33,6 +34,8 @@ void get_client_ip(int fd, char *buf, size_t len) {
 }
 
 static void release_sfd(struct socket_fd *sfd) {
+    //TODO:
+    //Add thread termination sfd->thread_id;
     close(sfd->fd);
     memset(sfd, 0xff, sizeof(struct socket_fd));
     free(sfd);
@@ -45,7 +48,7 @@ static void free_global_resources(void) {
         gfd.next = sfd->next;
         release_sfd(sfd);
     }
-        pthread_mutex_unlock(&g_sock_mutex);
+    pthread_mutex_unlock(&g_sock_mutex);
 }
 
 static int remove_global_fd(int fd) {
@@ -69,13 +72,70 @@ static int remove_global_fd(int fd) {
    return -1;
 }
 
-static void *receiving_thread (void *vargp) {
-    char buf[BUF_LEN_DEFAULT] = { 0, };
-    int socket_fd = *(int *)vargp;
+/*
+ * Decrypt function
+ * */
+static int decrypt(char *in, size_t in_len, char **out, size_t *out_len) {
+    char *ptr;
+    size_t len;
+    len = in_len + 1;
+    ptr = malloc(len);
+    if(!ptr)
+        return -1;
 
+    memcpy(ptr, in, in_len);
+    ptr[in_len] = '\0';
+
+    *out = ptr;
+    *out_len = len;
+
+    return 0;
+}
+
+/*
+ * Encrypt function
+ * */
+static int encrypt(char *in, size_t in_len, char **out, size_t *out_len) {
+    char *ptr;
+    size_t len;
+    len = in_len + 1;
+    ptr = malloc(len);
+    if(!ptr)
+        return -1;
+
+    memcpy(ptr, in, in_len);
+    ptr[in_len] = '\0';
+
+    *out = ptr;
+    *out_len = len;
+
+    return 0;
+}
+
+static int accept_message(int socket_fd, char **buf, size_t *len) {
+    char b[BUF_LEN_DEFAULT] = { 0, };
+    ssize_t l = read(socket_fd, b, sizeof(b));
+    if (l <= 0) {
+        if(l)
+            LOGE("Failed to read from socket %d", socket_fd);
+        else
+            LOGI("Connection from socket %d was closed by peer",
+                socket_fd);
+        return -1;
+    }
+
+    return decrypt(b, (size_t)l, buf, len);
+}
+
+static void *receiving_thread (void *vargp) {
+    int socket_fd = *(int *)vargp;
+    char *buf;
+    size_t len;
     while (1) {
-        if (read(socket_fd, buf, sizeof(buf)) > 0)
+        if (accept_message(socket_fd, &buf, &len) == 0) {
             fprintf(stdout, ">>%s\n", buf);
+            free(buf);
+        }
         else {
             LOGI("Closing cosket %d", socket_fd);
             remove_global_fd(socket_fd);
@@ -233,7 +293,7 @@ static void *main_listener_thread (void *vargp) {
         int session_fd = accept(listening_fd, NULL, NULL);
         if (session_fd <= 0) {
             LOGE("Failed to accept. something went wrong...");
-            return NULL;
+            pthread_exit(NULL);
         }
 
         if (handle_input_connection(session_fd))
@@ -262,13 +322,21 @@ static void handle_broadcasting_mode(void) {
     fprintf(stdout, "You are in chat mode. Enter '-exit' to exit"
                     " the mode.\n");
     while (1) {
+        char *buf;
+        size_t len;
         char message[BUF_LEN_DEFAULT] = { 0, };
         fill_buf_from_stdin(message, sizeof(message));
 
-        if(strstr(message, "-exit"))
+        if (strstr(message, "-exit"))
             return;
 
+        if (encrypt(message, strlen(message), &buf, &len)) {
+            LOGE("Failed to encrypt message");
+            continue;
+        }
+
         write_broadcasting_message(message, strlen(message));
+        free(buf);
     }
 }
 
@@ -298,8 +366,7 @@ static void print_usage(void) {
     fprintf(stdout, "    0 - exit \n");
 }
 
-static int main_loop(void)
-{
+static int main_loop(void) {
     print_usage();
     while (1) {
         int cmd = -1;
@@ -326,8 +393,7 @@ static int main_loop(void)
 
             default:
                 LOGE("Wrong command");
-                print_usage();
-                break;
+                return 0;
         }
     }
 
@@ -344,7 +410,6 @@ int main(void) {
     main_loop();
     free_global_resources();
     close(listener_fd);
-    pthread_cancel(m_listen_thread_id);
 
     return 0;
 }
